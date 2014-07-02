76e81f0c2563a0786f01e8962214bd34fd1aade7
Anita <anita060809@gmail.com>
2014-07-02 17:54:07 -0500

    Initial commit

---
+++ Estudio/Models/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Estudio.Models
+{
+    public class Alumno
+    {
+        public int AlumnoId { set; get; }
+
+        public string Nombre { set; get; }
+    }
+}
